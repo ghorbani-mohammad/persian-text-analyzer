3bd669d633a9099be31b6cd1726f0f6de56298e0
Mohammad <aali361@gmail.com>
2022-03-16 14:33:25 +0330

    feat: minor change

--- analyzer/analyzer/urls.py
+++ analyzer/analyzer/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('secret_admin/', admin.site.urls),
+    path('secret-admin/', admin.site.urls),
     path("ner/", views.NERExtractionAPIView.as_view()),
 ]
