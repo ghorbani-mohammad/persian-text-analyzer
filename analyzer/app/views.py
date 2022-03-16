7a8aa6e03b4c7aa95ef29053080f14844b865ad1
Mohammad <aali361@gmail.com>
2022-03-16 15:04:01 +0330

    feat: add ner extraction to view

--- analyzer/app/views.py
+++ analyzer/app/views.py
@@ -1,6 +1,15 @@
 from rest_framework import views
 from rest_framework.response import Response
 
+from . import apps
+from . import utils
+
 class NERExtractionAPIView(views.APIView):
     def post(self, request):
-        return Response({"keywords": "keywords", "keyphrases": "keyphrases"})
+        text = request.data['text']
+        ner_output = apps.AppConfig.ner_model(apps.AppConfig.normalizer.normalize(text))
+        if len(ner_output) > 0:
+            entities = utils.ner_report(ner_output)
+            print(entities)
+            return Response(entities)
+        return Response()
