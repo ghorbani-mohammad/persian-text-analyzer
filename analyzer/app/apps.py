2db20370fb1ef3718e70e62acaff4dcf93c524be
Mohammad <aali361@gmail.com>
2022-03-16 14:58:56 +0330

    feat: add ner-model to apps.py

--- analyzer/app/apps.py
+++ analyzer/app/apps.py
@@ -1,6 +1,17 @@
+import hazm
+from transformers import pipeline
+from transformers import AutoTokenizer
+from transformers import AutoModelForTokenClassification
+
 from django.apps import AppConfig
 
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+    normalizer = hazm.Normalizer()
+    model_name_or_path = 'ner_model'
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+    model = AutoModelForTokenClassification.from_pretrained(model_name_or_path)
+    ner_model = pipeline('ner', model=model, tokenizer=tokenizer)
