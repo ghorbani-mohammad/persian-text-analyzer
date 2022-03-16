fbf6f7fd1c014ac4217a397ebf759080f16fde01
Mohammad <aali361@gmail.com>
2022-03-16 14:35:13 +0330

    fix: minor change

--- analyzer/analyzer/views.py
+++ analyzer/analyzer/views.py
@@ -2,5 +2,5 @@ from rest_framework import views
 from rest_framework.response import Response
 
 class NERExtractionAPIView(views.APIView):
-    def post(self, request, version):
+    def post(self, request):
         return Response({"keywords": "keywords", "keyphrases": "keyphrases"})
