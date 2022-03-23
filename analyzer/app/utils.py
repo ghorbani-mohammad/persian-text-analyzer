69a113d67e852c14e64b08b0999bbc1416288bad
Mohammad <aali361@gmail.com>
2022-03-23 20:21:21 +0430

    feat: refactor code

--- analyzer/app/utils.py
+++ analyzer/app/utils.py
@@ -1,57 +1,57 @@
 def ner_report(outputs):
     tag_dict = {
-        "B-date": 'date',
-        "B-event": 'event',
-        "B-facility": 'facility',
-        "B-location": 'location',
-        "B-money": 'money',
-        "B-organization": 'organization',
-        "B-percent": 'percent',
-        "B-person": 'person',
-        "B-product": 'product',
-        "B-time": 'time',
-        "I-date": 'date',
-        "I-event": 'event',
-        "I-facility": 'facility',
-        "I-location": 'location',
-        "I-money": 'money',
-        "I-organization": 'organization',
-        "I-percent": 'percent',
-        "I-person": 'person',
-        "I-product": 'product',
-        "I-time": 'time',
+        "B-date": "date",
+        "B-event": "event",
+        "B-facility": "facility",
+        "B-location": "location",
+        "B-money": "money",
+        "B-organization": "organization",
+        "B-percent": "percent",
+        "B-person": "person",
+        "B-product": "product",
+        "B-time": "time",
+        "I-date": "date",
+        "I-event": "event",
+        "I-facility": "facility",
+        "I-location": "location",
+        "I-money": "money",
+        "I-organization": "organization",
+        "I-percent": "percent",
+        "I-person": "person",
+        "I-product": "product",
+        "I-time": "time",
         "O": "O",
     }
     report_entity = {
-        'date': [],
-        'event': [],
-        'facility': [],
-        'location': [],
-        'money': [],
-        'organization': [],
-        'percent': [],
-        'person': [],
-        'product': [],
-        'time': [],
+        "date": [],
+        "event": [],
+        "facility": [],
+        "location": [],
+        "money": [],
+        "organization": [],
+        "percent": [],
+        "person": [],
+        "product": [],
+        "time": [],
     }
     output = outputs[0]
-    output_entity = output['entity']
-    phrase_tag = tag_dict[output['entity']]
-    I_B = output_entity.split('-')
-    recognized_phrase = str(output['word'] + " ")
+    output_entity = output["entity"]
+    phrase_tag = tag_dict[output["entity"]]
+    I_B = output_entity.split("-")
+    recognized_phrase = str(output["word"] + " ")
     GO = True
     curser = 0
     while GO:
         curser += 1
         if curser != len(outputs):
-            output_entity = outputs[curser]['entity']
-            I_B = output_entity.split('-')
+            output_entity = outputs[curser]["entity"]
+            I_B = output_entity.split("-")
             if I_B[0] == "I" and I_B[1] == phrase_tag:
-                recognized_phrase += str(outputs[curser]['word'] + " ")
+                recognized_phrase += str(outputs[curser]["word"] + " ")
             if I_B[0] == "B":
                 report_entity[phrase_tag].append(recognized_phrase)
-                phrase_tag = tag_dict[outputs[curser]['entity']]
-                recognized_phrase = str(outputs[curser]['word'] + " ")
+                phrase_tag = tag_dict[outputs[curser]["entity"]]
+                recognized_phrase = str(outputs[curser]["word"] + " ")
         else:
             report_entity[phrase_tag].append(recognized_phrase)
             GO = False
--- analyzer/app/views.py
+++ analyzer/app/views.py
@@ -1,33 +1,38 @@
 from rest_framework import views
 from rest_framework.response import Response
 
-from . import apps
-from . import utils
+from . import apps, utils
+
 
 class NERExtractionAPIView(views.APIView):
     def post(self, request):
-        text = request.data['text']
+        text = request.data["text"]
         ner_output = apps.AppConfig.ner_model(apps.AppConfig.normalizer.normalize(text))
         if len(ner_output) > 0:
             entities = utils.ner_report(ner_output)
             return Response(entities)
         return Response()
 
+
 class SentimentAPIView(views.APIView):
     def post(self, request):
-        text = request.data['text']
-        result = apps.AppConfig.sentiment_model(apps.AppConfig.normalizer.normalize(text), return_all_scores=True)[0]
+        text = request.data["text"]
+        result = apps.AppConfig.sentiment_model(
+            apps.AppConfig.normalizer.normalize(text), return_all_scores=True
+        )[0]
         print(result)
         temp = {
-            'angry': result[0]['score']+result[1]['score'], 
-            'happy': result[3]['score']+result[4]['score'], 
-            'neutral': result[2]['score']
+            "angry": result[0]["score"] + result[1]["score"],
+            "happy": result[3]["score"] + result[4]["score"],
+            "neutral": result[2]["score"],
         }
         return Response(temp)
 
 
 class ClassificationAPIView(views.APIView):
     def post(self, request):
-        text = request.data['text']
-        result = apps.AppConfig.classification_model(apps.AppConfig.normalizer.normalize(text), return_all_scores=True)[0]
+        text = request.data["text"]
+        result = apps.AppConfig.classification_model(
+            apps.AppConfig.normalizer.normalize(text), return_all_scores=True
+        )[0]
         return Response(result)
