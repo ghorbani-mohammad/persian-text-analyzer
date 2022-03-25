39f3bca802e5cb73c12f2f149fefaa01ee9caef6
Mohammad <aali361@gmail.com>
2022-03-25 11:25:11 +0430

    feat: minor change

--- analyzer/app/apps.py
+++ analyzer/app/apps.py
@@ -9,21 +9,25 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app"
+    try:
+        # normalizer
+        normalizer = hazm.Normalizer()
 
-    # normalizer
-    normalizer = hazm.Normalizer()
+        # ner
+        tokenizer = AutoTokenizer.from_pretrained("/app/analyzer/ner_model")
+        model = AutoModelForTokenClassification.from_pretrained(
+            "/app/analyzer/ner_model"
+        )
+        ner_model = pipeline("ner", model=model, tokenizer=tokenizer)
 
-    # ner
-    tokenizer = AutoTokenizer.from_pretrained("/app/analyzer/ner_model")
-    model = AutoModelForTokenClassification.from_pretrained("/app/analyzer/ner_model")
-    ner_model = pipeline("ner", model=model, tokenizer=tokenizer)
+        # sentiment
+        sentiment_model = pipeline(
+            "sentiment-analysis", model="/app/analyzer/sentiment_model"
+        )
 
-    # sentiment
-    sentiment_model = pipeline(
-        "sentiment-analysis", model="/app/analyzer/sentiment_model"
-    )
-
-    # classification
-    classification_model = pipeline(
-        "text-classification", "/app/analyzer/classification_model"
-    )
+        # classification
+        classification_model = pipeline(
+            "text-classification", "/app/analyzer/classification_model"
+        )
+    except:
+        print("Error in loading models")
