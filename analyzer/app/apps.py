967b8f400a8f67a252627a1e2a37f6a79c375bc8
Mohammad <aali361@gmail.com>
2022-05-07 09:05:49 +0430

    feat: minor change

--- analyzer/app/apps.py
+++ analyzer/app/apps.py
@@ -12,19 +12,16 @@ class AppConfig(AppConfig):
     try:
         # normalizer
         normalizer = hazm.Normalizer()
-
         # ner
         tokenizer = AutoTokenizer.from_pretrained("/app/analyzer/ner_model")
         model = AutoModelForTokenClassification.from_pretrained(
             "/app/analyzer/ner_model"
         )
         ner_model = pipeline("ner", model=model, tokenizer=tokenizer)
-
         # sentiment
         sentiment_model = pipeline(
             "sentiment-analysis", model="/app/analyzer/sentiment_model"
         )
-
         # classification
         classification_model = pipeline(
             "text-classification", "/app/analyzer/classification_model"
