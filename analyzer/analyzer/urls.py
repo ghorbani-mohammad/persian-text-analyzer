2f049ba11aa5007acd4210e880f9c807e730886c
Mohammad <aali361@gmail.com>
2022-08-15 23:52:09 +0430

    add django admin title

--- analyzer/analyzer/urls.py
+++ analyzer/analyzer/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('secret-admin/', admin.site.urls),
+    path("secret-admin/", admin.site.urls),
     path("v1/app/", include("app.urls")),
 ]
+
+admin.site.index_title = "Persian Text"
+admin.site.site_title = "Persian Text Django Admin"
+admin.site.site_header = "Persian Text Django Administration Panel"
