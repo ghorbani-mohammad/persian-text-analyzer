7f5a999bd958c21eace9b1b5d483f8ea6d4ec5fd
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-05 20:59:43 +0330

    minor change

--- analyzer/app/urls.py
+++ analyzer/app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("ner/", views.NERExtractionAPIView.as_view()),
     path("keyword/", views.KeywordAPIView.as_view()),
+    path("ner/", views.NERExtractionAPIView.as_view()),
     path("sentiment/", views.SentimentAPIView.as_view()),
     path("classification/", views.ClassificationAPIView.as_view()),
 ]
